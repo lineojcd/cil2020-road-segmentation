8e989a11704b480ca08056e81fa6af5de65ce50a
lineojcd <45730495+lineojcd@users.noreply.github.com>
2020-07-31 20:20:28 +0200

    Add files via upload

---
+++ core/seg_opr/seg_oprs.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# @Time    : 2018/6/17 上午12:43
+# @Author  : yuchangqian
+# @Contact : changqian_yu@163.com
+# @File    : seg_oprs.py
+from collections import OrderedDict
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+# auxilary class for combining Conv2d, BatchNorm2d and ReLU 
+class ConvBnRelu(nn.Module):
+    def __init__(self, in_planes, out_planes, ksize, stride, pad, dilation=1,
+                 groups=1, has_bn=True, norm_layer=nn.BatchNorm2d, bn_eps=1e-5, 
+                 has_relu=True, inplace=True, has_bias=False):
+        super(ConvBnRelu, self).__init__()
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=ksize,
+                              stride=stride, padding=pad,
+                              dilation=dilation, groups=groups, bias=has_bias)
+        
+        self.has_bn = has_bn
+        if self.has_bn:
+            self.bn = norm_layer(out_planes, eps=bn_eps)
+        
+        self.has_relu = has_relu
+        if self.has_relu:
+            self.relu = nn.ReLU(inplace=inplace)
+            
+    def forward(self, x):
+        x = self.conv(x)
+        if self.has_bn:
+            x = self.bn(x)
+        if self.has_relu:
+            x = self.relu(x)
+            
+        return x
+
+# ARM module mention in report
+
+class AttentionRefinement(nn.Module):
+    def __init__(self, in_planes, out_planes,
+                 norm_layer=nn.BatchNorm2d):
+        super(AttentionRefinement, self).__init__()
+        self.conv_3x3 = ConvBnRelu(in_planes, out_planes, 3, 1, 1,
+                                   has_bn=True, norm_layer=norm_layer,
+                                   has_relu=True, has_bias=False)
+        self.channel_attention = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            ConvBnRelu(out_planes, out_planes, 1, 1, 0,
+                       has_bn=True, norm_layer=norm_layer,
+                       has_relu=False, has_bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        fm = self.conv_3x3(x)
+        fm_se = self.channel_attention(fm)
+        fm = fm * fm_se
+
+        return fm
+
+
+# Feature Fusion FFM mentioned in the report    
+class FeatureFusion(nn.Module):
+    def __init__(self, in_planes, out_planes,
+                 reduction=1, norm_layer=nn.BatchNorm2d):
+        super(FeatureFusion, self).__init__()
+        self.conv_1x1 = ConvBnRelu(in_planes, out_planes, 1, 1, 0,
+                                   has_bn=True, norm_layer=norm_layer,
+                                   has_relu=True, has_bias=False)
+        self.channel_attention = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            ConvBnRelu(out_planes, out_planes // reduction, 1, 1, 0,
+                       has_bn=False, norm_layer=norm_layer,
+                       has_relu=True, has_bias=False),
+            ConvBnRelu(out_planes // reduction, out_planes, 1, 1, 0,
+                       has_bn=False, norm_layer=norm_layer,
+                       has_relu=False, has_bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x1, x2):
+        fm = torch.cat([x1, x2], dim=1)
+        fm = self.conv_1x1(fm)
+        fm_se = self.channel_attention(fm)
+        output = fm + fm * fm_se
+        return output
